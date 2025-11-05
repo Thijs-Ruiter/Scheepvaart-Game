c94437062e847498df8bc15b816b71fccdd0fdc8
Thijs Ruiter <thijs.ruiter@coderclass.nl>
2025-11-05 10:28:14 +0100

    Worked on Movement Script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,26 +7,28 @@ public class Movement : MonoBehaviour
         None, Selected
     }
     private TroopState state;
-    private Event e = Event.current;
 
-    //private Vector2 target = transform.position;
+    private Vector2 target;
+    public float speed = 5;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        target = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Event e = Event.current;
         switch (state)
         {
             case TroopState.None:
+                transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * speed);
                 break;
             case TroopState.Selected:
                 if (e.button == 0)
                 {
-                    
+                    target = e.mousePosition;
                     state = TroopState.None;
                 }
                 break;
@@ -36,9 +38,15 @@ public class Movement : MonoBehaviour
 
     private void OnMouseDown()
     {
+        UnityEngine.Debug.Log("I got Clicked");
         if (state != TroopState.Selected)
         {
             state = TroopState.Selected;
         }
+    }
+
+    private void OnMouseOver()
+    {
+        
     }
 }
